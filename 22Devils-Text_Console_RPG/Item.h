01b6550bcbb2043d28580c54a5de7a8ba7f917b9
ohchaeyul\olikk <olikkaan9108@gmail.com>
2026-03-27 14:44:16 +0900

    아이템 주석 제거 후 푸시

--- 22Devils-Text_Console_RPG/Item.h
+++ 22Devils-Text_Console_RPG/Item.h
@@ -10,12 +10,15 @@ private:
 
 public:
 
+	void ShowAllItems();
+
 	void RareCandy(Player& player);
 	void HPPotion(Player& player);
 	void FHPPotion(Player& player);
 	void MultiLens(Player& player);
 	void PowerPotion(Player& player);
-
+	void randomexp(Player& player);
+	void randomgold(Player& player);
 
 
 };
